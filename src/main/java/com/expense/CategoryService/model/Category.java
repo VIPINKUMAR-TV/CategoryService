456882c0f6c7082613ab0671afee6763a430202b
Vipinkumar <tvkumarvipin@gmail.com>
2026-01-21 20:50:05 +0530

    model class updated with more annotations

--- src/main/java/com/expense/CategoryService/model/Category.java
+++ src/main/java/com/expense/CategoryService/model/Category.java
@@ -1,17 +1,24 @@
 package com.expense.CategoryService.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "categories")
 public class Category {
