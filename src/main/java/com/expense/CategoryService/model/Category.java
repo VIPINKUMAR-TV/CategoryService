8c477eaa8ab5424bc5b54420791876e3f71c32c2
Vipinkumar <tvkumarvipin@gmail.com>
2026-01-22 13:18:35 +0530

    changes to id generator error

--- src/main/java/com/expense/CategoryService/model/Category.java
+++ src/main/java/com/expense/CategoryService/model/Category.java
@@ -1,10 +1,12 @@
 package com.expense.CategoryService.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,8 +24,14 @@ import lombok.ToString;
 @Entity
 @Table(name = "categories", schema = "categorydb")
 public class Category {
-	  @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cat_seq")
+	@SequenceGenerator(
+	    name = "cat_seq",
+	    sequenceName = "categorydb.category_seq",
+	    allocationSize = 1
+	)
+	@Column(name = "cat_id")
 	    private Long catId;
 	  
 	  	private Long userId;
